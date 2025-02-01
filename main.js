dd06567949f8dc323954c2ababa1a6461b6a2263
Tomi <tomasagustinbrizuela@gmial.com>
2025-02-01 10:26:54 -0300

    First commit

---
+++ main.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const helment = require('helmet');
+const morngan = require('morgan');
+const cors = require('cors');
+const PORT = 3030;
+
+
+const app = express();
+
+app.use(cors())
+app.use(helment())
+app.use(morngan('dev'))
+app.use(express.json())
+
+app.get("/health", (req, res) => {
+    try {
+        return res.status(200).json({ "Message": "Running healthy" })
+    } catch (error) {
+        return res.status(400).json({ "Error": "Something went wrong" })
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`);
+});
